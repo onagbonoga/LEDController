44496bbf2e7dc64fc1269f289084027df7334d35
Nurudeen Agbonoga <nurudeenagbonoga@gmail.com>
2023-05-23 16:49:43 -0500

    updated app.py to parse CL arguments

--- LEDController/app.py
+++ LEDController/app.py
@@ -1,4 +1,26 @@
-from LEDApp import app
+from LEDApp import app 
+import optparse 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    default_port = "80"
+    default_host = "0.0.0.0"
+    parser = optparse.OptionParser()
+    parser.add_option("-H", "--host",
+                      help=f"Hostname of Flask app {default_host}.",
+                      default=default_host)
+
+    parser.add_option("-P", "--port",
+                      help=f"Port for Flask app {default_port}.",
+                      default=default_port)
+
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug",
+                      help=optparse.SUPPRESS_HELP)
+
+    options, _ = parser.parse_args()
+
+    app.run(
+        debug=options.debug,
+        host=options.host,
+        port=int(options.port)
+    )
