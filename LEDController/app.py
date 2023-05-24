cef5a5af664649277604698236036cbb59cec0ac
Nurudeen Agbonoga <nurudeenagbonoga@gmail.com>
2023-05-23 20:43:14 -0500

    updated file structure

--- LEDController/app.py
+++ LEDController/app.py
@@ -1,5 +1,33 @@
-from LEDApp import app 
 import optparse 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+class LED():
+    def __init__(self):
+        self.R = 50
+        self.G = 50
+        self.B = 50
+    def set_values(self,data):
+        self.R = int(data['R'])
+        self.G = int(data['G'])
+        self.B = int(data['B'])
+
+LED_data = LED()
+@app.route('/',methods=('GET','POST'))
+def index():
+    if request.method == "POST":
+        data = request.get_json() #recieve request
+        if data['action'] == 1: #  write to the LED data object
+            LED_data.set_values(data)
+
+        elif int(data['action']) == 0: # return the values for LED data as hex code
+            result = "*{:02X}{:02X}{:02X}".format(LED_data.R, LED_data.G, LED_data.B)
+            
+            return result
+
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     default_port = "80"
